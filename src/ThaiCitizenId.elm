7377452daa02db64a194c1365f71ccddc5db6d2a
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-12-22 18:21:17 +0700

    Add more docs comment

--- src/ThaiCitizenId.elm
+++ src/ThaiCitizenId.elm
@@ -1,10 +1,6 @@
 module ThaiCitizenId exposing (CitizenId, validate)
 
-type alias CitizenId = String
-
-{-| Validate citizen id card.
-
-Refs:
+{-| It's library uses for validate thai citizen id. This library implements base on:
 
   - <https://www.memo8.com/%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%84%E0%B8%B3%E0%B8%99%E0%B8%A7%E0%B8%93%E0%B9%80%E0%B8%9E%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%95%E0%B8%A3%E0%B8%A7%E0%B8%88%E0%B8%AA/>
   - <https://github.com/codeforthailand/Thai-Citizen-ID-Validator>
@@ -12,6 +8,14 @@ Refs:
 -}
 
 
+{-| CitizenId it's a String type to represents citizen id.
+-}
+type alias CitizenId =
+    String
+
+
+{-| Validate citizen id card.
+-}
 validate : CitizenId -> Bool
 validate id =
     thirteenLength id && validateId id
