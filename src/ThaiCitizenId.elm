387a8f3a1c475ae8db2405342bcedabb8ae15493
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-12-22 18:23:04 +0700

    Add more docs comment

--- src/ThaiCitizenId.elm
+++ src/ThaiCitizenId.elm
@@ -5,6 +5,12 @@ module ThaiCitizenId exposing (CitizenId, validate)
   - <https://www.memo8.com/%E0%B8%A7%E0%B8%B4%E0%B8%98%E0%B8%B5%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%84%E0%B8%B3%E0%B8%99%E0%B8%A7%E0%B8%93%E0%B9%80%E0%B8%9E%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%95%E0%B8%A3%E0%B8%A7%E0%B8%88%E0%B8%AA/>
   - <https://github.com/codeforthailand/Thai-Citizen-ID-Validator>
 
+# Type
+@docs CitizenId
+
+# Validation
+@docs validate
+
 -}
 
 
